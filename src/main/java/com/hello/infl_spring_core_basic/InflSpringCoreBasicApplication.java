14f7ce900998e60574d2a083d9378966115bd7d3
mino4084 <mino4084@kyobobook.com>
2024-03-05 08:15:32 +0900

    Initial commit

---
+++ src/main/java/com/hello/infl_spring_core_basic/InflSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package com.hello.infl_spring_core_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflSpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflSpringCoreBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/infl_spring_core_basic/InflSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.infl_spring_core_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflSpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
