c0c9cfc60e216a7eb3513afe59e23a9e9b18a5f6
mino4084 <mino4084@kyobobook.com>
2024-03-14 07:37:32 +0900

    스프링 컨테이너 기반의 싱글톤 사용 및 테스트

--- src/test/java/com/hello/infl_spring_core_basic/singleton/SingletonTest.java
+++ src/test/java/com/hello/infl_spring_core_basic/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import com.hello.infl_spring_core_basic.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,5 +57,33 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         //same -> 객체 인스턴스를 비교
         //eqaul -> 단순 값 비교
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너로 싱글톤 테스트")
+    void springContainer() {
+
+        // 스프링 컨테이너 사용
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 호출할 때마다 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        // 2. 호출할 때마다 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // 동일한 객체 인스턴스
+        // 스프링 컨테이너 사용으로 이미 만들어진 객체를 공유해서 효율적으로 재사용이 가능
+        // 요청이 올때 마다 하나의 객체로 사용
+        // memberService1 = com.hello.infl_spring_core_basic.member.MemberServiceImpl@29ca3d04
+        // memberService2 = com.hello.infl_spring_core_basic.member.MemberServiceImpl@29ca3d04
+
+        // assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isSameAs(memberService2);
+
+        // 일반적인 웹 애플리케이션에서는 무수히 많은 요청이 생성되므로, 요청이 올때마다 객체가 생성되고 소멸되니 메모리 낭비 발생
     }
 }
