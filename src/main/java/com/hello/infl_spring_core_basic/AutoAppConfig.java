784be8867bcb95c465281337dcf63649f2574823
mino4084 <mino4084@kyobobook.com>
2024-03-20 07:53:27 +0900

    컴포넌트 스캔 탐색 위치와 스캔 대상

--- src/main/java/com/hello/infl_spring_core_basic/AutoAppConfig.java
+++ src/main/java/com/hello/infl_spring_core_basic/AutoAppConfig.java
@@ -17,9 +17,21 @@ import org.springframework.context.annotation.FilterType;
  */
 
 @Configuration
-@ComponentScan(excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-})
+@ComponentScan(
+        // 탐색 위치를 지정하지 않으면 모든 패키지를 다 탐색한다. 심지어 라이브러리까지
+        basePackages = {"com.hello.infl_spring_core_basic.member"},
+        // 지정한 클래스 위치의 하위 패키지부터 탐색
+        basePackageClasses = {AutoAppConfig.class},
+        // basePackages, basePackageClasses 모두 지정하지 않으면
+        // package com.hello.infl_spring_core_basic; 탐색
+        // 그래서 별도 지정하지않고 설정정보 클래스 위치를 프로젝트 최상단에 위치한다.
+        // 애플리케이션 클래스를 보면 @SpringBootApplication 해당 어노테이션이 명시
+        // @SpringBootApplication 안에 @ComponentScan이 들어가있음.
+        excludeFilters = {
+                // 기존 코드 AppConfig.class 사용하지 않기 위한 역할
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        }
+)
 // @Component 붙은 모든 클래스를 스프링 빈으로 등록
 // 스프링 빈의 기본 이름은 클래스명을 사용하지만, 맨 앞글자만 소문자로 사용한다
 // MemberServiceImpl -> memberServiceImpl
